bf8107f4f15e43693cff12517be89b8cecb86e7e
Olly <ollyt1998@gmail.com>
2021-07-31 14:40:09 +1200

    added headers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Button} from "@material-ui/core";
 import './App.css';
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Button color="primary">CHA TM</Button>
+
     </div>
   );
 }
